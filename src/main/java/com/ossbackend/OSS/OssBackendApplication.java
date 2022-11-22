49687f6fb50746a0b7bab09cb827caaf587e2a32
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-11-21 20:32:50 -0600

    file setup

---
+++ src/main/java/com/ossbackend/OSS/OssBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ossbackend.OSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ossbackend/OSS/OssBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ossbackend.OSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
